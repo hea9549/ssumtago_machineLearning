458c199f2cca68f597f1bd56d17f41bb8ab9656e
jun <leebduk@gmail.com>
2017-05-17 21:59:05 +0900

    added xiver initialization, 10 layers, drop out

--- confess_success/nn/relu/nn_relu_when_to_confess.py
+++ confess_success/nn/relu/nn_relu_when_to_confess.py
@@ -6,6 +6,8 @@ from numpy import genfromtxt
 from sklearn.model_selection import train_test_split
 import matplotlib.pylab as plt
 # tf.set_random_seed(777)  # for reproducibility
+tf.set_random_seed(777)
+keep_prob = tf.placeholder(tf.float32)
 
 my_data = genfromtxt("../../data/confess_success_434.csv", delimiter=',')
 
@@ -14,53 +16,74 @@ y_data = my_data[:,-1:].tolist()
 
 x_num_of_feature = len(x_data[0])
 
-x_train_data, x_test_data , y_train_data, y_test_data=train_test_split(x_data,y_data,test_size=0.1,random_state=20)
+x_train_data, x_test_data , y_train_data, y_test_data=train_test_split(x_data,y_data,test_size=0.20,random_state=40)
 print(len(x_train_data))
-
+num_of_unit = 64
 X = tf.placeholder(tf.float32, shape=[None, x_num_of_feature])
 Y = tf.placeholder(tf.float32, shape=[None, 1])
 
-W1 = tf.get_variable("W1", shape=[x_num_of_feature, x_num_of_feature])
-b1 = tf.Variable(tf.random_normal([x_num_of_feature]))
+W1 = tf.get_variable("W1", shape=[x_num_of_feature, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b1 = tf.Variable(tf.random_normal([num_of_unit]))
 L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+L1 = tf.nn.dropout(L1, keep_prob)
 
-W2 = tf.get_variable("W2", shape=[x_num_of_feature, x_num_of_feature])
-b2 = tf.Variable(tf.random_normal([x_num_of_feature]))
+W2 = tf.get_variable("W2", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b2 = tf.Variable(tf.random_normal([num_of_unit]))
 L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+L2 = tf.nn.dropout(L2, keep_prob)
 
-W3 = tf.get_variable("W3", shape=[x_num_of_feature, x_num_of_feature])
-b3 = tf.Variable(tf.random_normal([x_num_of_feature]))
+W3 = tf.get_variable("W3", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b3 = tf.Variable(tf.random_normal([num_of_unit]))
 L3 = tf.nn.relu(tf.matmul(L2, W3) + b3)
+L3 = tf.nn.dropout(L3, keep_prob)
 
-W4 = tf.get_variable("W4", shape=[x_num_of_feature, x_num_of_feature])
-b4 = tf.Variable(tf.random_normal([x_num_of_feature]))
+W4 = tf.get_variable("W4", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b4 = tf.Variable(tf.random_normal([num_of_unit]))
 L4 = tf.nn.relu(tf.matmul(L3, W4) + b4)
+L4 = tf.nn.dropout(L4, keep_prob)
 
-W5 = tf.get_variable("W5", shape=[x_num_of_feature, 1])
-b5 = tf.Variable(tf.random_normal([1]))
-hypothesis = tf.sigmoid(tf.matmul(L4, W5) + b5)
+W5 = tf.get_variable("W5", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b5 = tf.Variable(tf.random_normal([num_of_unit]))
+# hypothesis = tf.sigmoid(tf.matmul(L4, W5) + b5)
+# hypothesis = tf.sigmoid(L4, W5) + b5)
+L5 = tf.nn.relu(tf.matmul(L4, W5) + b5)
+L5 = tf.nn.dropout(L5, keep_prob)
 
-# W6 = tf.get_variable("W6", shape=[x_num_of_feature, x_num_of_feature])
-# b6 = tf.Variable(tf.random_normal([x_num_of_feature]))
-# L6 = tf.nn.relu(tf.matmul(L5, W6) + b6)
-#
-# W7 = tf.get_variable("W7", shape=[x_num_of_feature, x_num_of_feature])
-# b7 = tf.Variable(tf.random_normal([x_num_of_feature]))
-# L7 = tf.nn.relu(tf.matmul(L6, W7) + b7)
-#
-# W8 = tf.get_variable("W8", shape=[x_num_of_feature, 1])
-# b8 = tf.Variable(tf.random_normal([1]))
-# hypothesis = tf.sigmoid(tf.matmul(L7, W8) + b8)
+W6 = tf.get_variable("W6", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b6 = tf.Variable(tf.random_normal([num_of_unit]))
+L6 = tf.nn.relu(tf.matmul(L5, W6) + b6)
+L6 = tf.nn.dropout(L6, keep_prob)
 
-# W9 = tf.get_variable("W9", shape=[x_num_of_feature, 1])
-# b9 = tf.Variable(tf.random_normal([1]))
-# hypothesis = tf.sigmoid(tf.matmul(L8, W9) + b9)
+
+W7 = tf.get_variable("W7", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b7 = tf.Variable(tf.random_normal([num_of_unit]))
+L7 = tf.nn.relu(tf.matmul(L6, W7) + b7)
+L7 = tf.nn.dropout(L7, keep_prob)
+
+W8 = tf.get_variable("W8", shape=[num_of_unit, num_of_unit],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b8 = tf.Variable(tf.random_normal([num_of_unit]))
+L8 = tf.nn.relu(tf.matmul(L7, W6) + b6)
+L8 = tf.nn.dropout(L8, keep_prob)
+
+W9 = tf.get_variable("W9", shape=[num_of_unit, 1],
+                     initializer=tf.contrib.layers.xavier_initializer())
+b9 = tf.Variable(tf.random_normal([1]))
+
+hypothesis = tf.sigmoid(tf.matmul(L8, W9) + b9)
 
 # cost/loss function
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
                        tf.log(1 - hypothesis))
 
-train = tf.train.GradientDescentOptimizer(learning_rate=0.05).minimize(cost)
+train = tf.train.AdamOptimizer(learning_rate=0.00001).minimize(cost)
 
 # Accuracy computation
 # True if hypothesis>0.5 else False
@@ -73,20 +96,21 @@ with tf.Session() as sess:
     accuracy_train_list = []
     accuracy_test_list = []
     step_list = []
-    for step in range(10001):
-        cost_val, _ = sess.run([cost, train], feed_dict={X: x_train_data, Y: y_train_data})
+    for step in range(100001):
+        cost_val, _ = sess.run([cost, train], feed_dict={X: x_train_data, Y: y_train_data, keep_prob: 0.7})
 
-        if step % 100 == 0:
+        if step % 500 == 0:
             step_list.append(step)
-            print(step, cost_val)
+
             h, c, train_a = sess.run([hypothesis, predicted, accuracy],
-                               feed_dict={X: x_train_data, Y: y_train_data})
+                               feed_dict={X: x_train_data, Y: y_train_data, keep_prob: 1})
             accuracy_train_list.append(train_a)
             h, c, a = sess.run([hypothesis, predicted, accuracy],
-                               feed_dict={X: x_test_data, Y: y_test_data})
+                               feed_dict={X: x_test_data, Y: y_test_data, keep_prob: 1})
             # print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy: ", a, "Real (Y)", y_test_data)
             accuracy_test_list.append(a)
 
+            print(step, cost_val,train_a,a)
             if train_a > 0.95:
                  break
 
